e6061aaef632e4fc90fef39058892c8cbc2a94c6
juhahinkula <hinkula@gmail.com>
2023-01-04 13:49:46 +0200

    customers query

--- src/Customers.js
+++ src/Customers.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 
 function Customers() {
+  const [customers, setCustomers] = useState([]);
+
+  const queryClient = useQueryClient();
+
+	const getCustomers = async () => {
+		const res = await fetch('https://customerrest.herokuapp.com/api/customers');
+	  const data =  await res.json();
+    return data.content;
+	};
+
+  const { data, error, isLoading } = useQuery({ queryKey: ['customers'], queryFn: getCustomers })
+
+  if (error) return <div>Request Failed</div>;
+	if (isLoading) return <div>Loading...</div>;
+
   return(
     <>
       Hello
